d12eee259b8381f87ebbab34524335950db3d15a
Ryo <kanadajinlee@yahoo.ca>
2016-09-23 23:02:47 +0000

    Adds conditionals exercises

---
+++ age-calculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  age == currentYear - yearOfBirth
+  return age
+}
+
+console.log(ageCalculator("Ryo", 1987, 2016));
+console.log(ageCalculator("Miranda", 1983, 2016));
+console.log(ageCalculator("Ferdinand", 1988, 2016));
